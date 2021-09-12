60b86fad0472f014a67db213720a02373dad7c11
sonya-ninja <hello@sonya.ninja>
2021-09-12 23:01:13 +0200

    start of responsive section

---
+++ src/lib/components/sections/SectionTwoCol.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let bgColor;
+</script>
+
+<section class="{bgColor}">
+    <div>
+        <p>LEFT COL</p>
+    </div>
+    <div>
+        <p>RIGHT COL</p>
+    </div>
+</section>
+
+<style lang="scss">
+    section {
+        // SMALL DEVICES
+        div {
+            padding: $spaceMD;
+            border: 1px solid red;
+            p {
+                height: 300px;
+            }
+        }
+        &.light1 {
+            background-color: $light1;
+        }
+        &.light2 {
+            background-color: $light2;
+        }
+        // MED - LARGE SCREENS
+        @media (min-width: $md) {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: $spaceXL;
+        }
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,21 @@
 <script>
     import HeroSlider from "$lib/components/HeroSlider.svelte";
     import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
+    import SectionTwoCol from "$lib/components/sections/SectionTwoCol.svelte";
 </script>
 
 
 <div class="pageWrap" in:slideFadeIn out:slideFadeOut>
     <HeroSlider></HeroSlider>
-    <section>
-        Home page here
-    </section>
+
+     <!-- Jill story -->
+    <SectionTwoCol bgColor="light1"></SectionTwoCol>
+
+    <!-- Banner -->
+    <SectionTwoCol bgColor="light2"></SectionTwoCol>
 </div>
 
 
 
 <style lang="scss">
-    section {
-        padding: $spaceLG;
-        margin: $spaceLG;
-        @include themewrap0;
-        :global(ul) {
-            list-style-type: none;
-        }
-    }
 </style>
