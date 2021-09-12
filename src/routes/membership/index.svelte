d27bb49440232ef5d8759b072ca82cd9459e6239
sonya-ninja <hello@sonya.ninja>
2021-09-12 21:26:01 +0200

    membership route

---
+++ src/routes/membership/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
+</script>
+
+
+<div class="pageWrap" in:slideFadeIn out:slideFadeOut>
+    <Header>MEMBERSHIP</Header>
+    <section>
+        <h3>PLACEHOLDER</h3>
+    </section>
+</div>
+
+
+
+<style lang="scss">
+    section {
+        padding: $spaceLG;
+        margin: $spaceLG;
+        @include themewrap0;
+        :global(ul) {
+            list-style-type: none;
+        }
+    }
+</style>
