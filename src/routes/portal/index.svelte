f7f4e268d14b9f68e6499296ba6bb0c675bd74f0
sonya-ninja <hello@sonya.ninja>
2021-11-04 12:30:53 +0200

    Temporary content

---
+++ src/routes/portal/index.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Header from '$lib/components/Header.svelte';
+    import { slideFadeIn, slideFadeOut } from "$lib/animation/transition-slideFade";
+</script>
+
+
+<div class="pageWrap" in:slideFadeIn out:slideFadeOut>
+    <Header>PORTAL</Header>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+</div>
+
+
+
+<style lang="scss">
+    section {
+        padding: $space2;
+        max-width: 100%;
+    }
+</style>
