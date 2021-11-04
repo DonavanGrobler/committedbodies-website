c80d145d6c2f9f608fe0e4e8f0dc80910e466f44
sonya-ninja <hello@sonya.ninja>
2021-11-04 10:24:52 +0200

    Temporary fix

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,9 @@
 <script>
     import BreadCrumbs from "./navigation/BreadCrumbs.svelte";
-    export let headerImage = "https://res.cloudinary.com/sonya-ninja/image/upload/t_pageHeader/v1631293309/committedbodies/gym/Gym-in-Benoni_lkncqu.jpg";
 </script>
 
 
 <header>
-    <img src="{headerImage}" alt="">
     <div class="titleWrap">
         <h1>
             <slot />
@@ -16,21 +14,20 @@
 
 <style lang="scss">
     header {
+        padding-top: 120px;
         position: relative;
         width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
-        img {
-            width: 100%;
-        }
+        background-image: $brandGradient;
         .titleWrap {
-            position: absolute;
+            padding: $space4;
             z-index: 3;
-            color: $transparent_light1;
+            color: $fontColorLight1;
             h1 {
-                color: #ffffff;
-                font-family: 'Rajdhani', sans-serif;
+                color: $fontColorLight3;
+                font-family: $font_boldEmphasis;
                 font-size: 5.143em;
                 font-weight: 700;
                 font-style: normal;
@@ -41,16 +38,6 @@
                 padding: 0;
                 margin: 0;
             }
-        }
-        &:before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            // background-color: rgba(0,0,0,0.6);
-            background-image: linear-gradient(127deg, rgb(0 0 0 / 70%), 5%, rgb(0 0 0 / 40%));
         }
 
     }
