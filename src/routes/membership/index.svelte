8fb7e0496067775d0514701929cb07a6d8464481
sonya-ninja <hello@sonya.ninja>
2021-11-04 10:27:34 +0200

    Temporary fix

--- src/routes/membership/index.svelte
+++ src/routes/membership/index.svelte
@@ -7,7 +7,32 @@
 <div class="pageWrap" in:slideFadeIn out:slideFadeOut>
     <Header>MEMBERSHIP</Header>
     <section>
-        <h3>PLACEHOLDER</h3>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
+    </section>
+    <section>
+        <h2>PLACEHOLDER</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, inventore recusandae. Nostrum explicabo quibusdam cumque nam beatae consequuntur. Necessitatibus beatae maiores nostrum cupiditate unde sequi aperiam omnis nesciunt ratione sit.</p>
     </section>
 </div>
 
@@ -15,11 +40,7 @@
 
 <style lang="scss">
     section {
-        padding: $spaceLG;
-        margin: $spaceLG;
-        @include themewrap0;
-        :global(ul) {
-            list-style-type: none;
-        }
+        padding: $space3;
+        margin: $space3;
     }
 </style>
