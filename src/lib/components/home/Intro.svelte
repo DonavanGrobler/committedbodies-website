e93f65b992382a2e9a4b0d7bd57ae513ea8d0ffc
sonya-ninja <hello@sonya.ninja>
2021-11-18 17:24:08 +0200

    layered images

--- src/lib/components/home/Intro.svelte
+++ src/lib/components/home/Intro.svelte
@@ -1,63 +1,55 @@
 <script>
-    export let introSections;
-    let intro = introSections[0];
+	export let introSections;
+	let intro = introSections[0];
 </script>
 
-<header>
-    <h1>{intro.title}</h1>
-    <p class="subtitle h4">{intro.subtitle}</p>
-</header>
-<section class="intro background2">
-    <img class="imgBefore" src="{intro.imageBefore}" alt="">
-    <img class="imgAfter" src="{intro.imageAfter}" alt="">
-    <div class="contentPrimary">
-        {@html intro.text}
-        <!-- {#if intro.buttonText}
-            <a class="button ghost" href="{intro.buttonLink}">{intro.buttonText}</a>
-        {/if} -->
-    </div>
+<section class="intro">
+	<img class="layer beforeLayer1" src="/assets/images/BEFORE/beforeBG.png" alt="">
+	<img class="layer beforeLayer4" src="/assets/images/BEFORE/beforeMirror.png" alt="">
+	<img class="layer beforeLayer3" src="/assets/images/BEFORE/beforeScale.png" alt="">
+	<img class="layer beforeLayer2" src="/assets/images/BEFORE/beforeJill.png" alt="">
+	
+	
+	
+	
+	
+
+
+	<!-- 
+		<div class="contentPrimary">
+		<h1>{intro.title}</h1>
+		<p class="subtitle h2">
+			{intro.subtitle}
+		</p>
+		{@html intro.text}
+		</div>
+		<img class="imgAfter" src="{intro.imageAfter}" alt="Jill Myburgh in a bikini on stage looking like a super-model after her weight-loss"> -->
 </section>
 
-<style lang="scss">
-    header {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 6vw 0 0 0;
-        h1 {
-            font-size: 4em;
-            text-align: center;
-        }
-        .subtitle {
-            font-family: $font_cursiveEmphasis;
-            font-size: 1.5em;
-            margin-top: 0;
-        }
-    }
-	section {
-        // border: 1px solid red;
-		max-width: 70em;
-    	margin: 0 auto;
-    }
-    section.intro {
-        overflow: auto;
-        .imgBefore {
-            float: left;
-            width: 35%;
-            shape-outside: url(https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto/v1635584514/ui-assets/beforeFront2_s0blvc.png);
-            shape-margin: 2em;
-        }
-        .imgAfter {
-            float: right;
-            width: 35%;
-            shape-outside: url(https://res.cloudinary.com/committed-bodies/image/upload/f_auto,q_auto/v1635583370/ui-assets/afterFront_2_up6xo9.png);
-            shape-margin: 2em;
-        }
-        .contentPrimary {
-            margin-top: 6em;
-            text-align: center;
-        }
-    }
 
+<style lang="scss">
+	
+	section.intro {
+		position: relative;
+		min-height: 100%;
+		width: 100%;
+		max-width: unset;
+		border: 1px solid red;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		align-items: flex-start;
+		.layer {
+			position: absolute;
+			max-width: unset;
+		}
+		
+		:global(.signature) {
+			color: hsl(var(--accentHS) var(--accentL));
+			font-family: $font_cursiveEmphasis;
+			font-size: var(--fontSizeH3);
+			margin: 0;
+		}
+	}
+	
 </style>
