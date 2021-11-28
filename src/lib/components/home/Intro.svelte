74156d0a813bcf2cc0855bf7e6304ef5e1d525bf
sonya-ninja <hello@sonya.ninja>
2021-11-28 08:16:08 +0200

    Jill Section not great but okay

--- src/lib/components/home/Intro.svelte
+++ src/lib/components/home/Intro.svelte
@@ -1,97 +1,116 @@
 <!-- ******HTML -->
 
-<section class="introTitle">
-	<h1 class="xxxl">{intro.title}</h1>
-	<p class="subtitle h2">{intro.subtitle}</p>
-</section>
-
-<section class="introBefore">
-	<div class="graphicsWrap">
-		<img class="layer beforeLayerBG" src="/assets/images/BEFORE/beforeBG.png" alt="">
-		<img class="layer beforeLayerMirror" src="/assets/images/BEFORE/beforeMirror.png" alt="">
-		<img class="layer beforeLayerScale" src="/assets/images/BEFORE/beforeScale.png" alt="">
-		<img class="layer beforeLayerJill" src="/assets/images/BEFORE/beforeJill.png" alt="">
+<section class="intro">
+	
+	<div class="introTitle">
+		<h1 class="xxxl">{intro.title}</h1>
+		<p class="subtitle h2">{intro.subtitle}</p>
 	</div>
 	
 	<div class="content">
-		{@html intro.text}
+		<div class="graphicsWrap">
+			<img class="layer layerBG" src="/assets/images/Before-After/BG.png" alt="">
+			<img class="layer layerBeforeTextBG" src="/assets/images/Before-After/BeforeTextBG.png" alt="">
+			<img class="layer layerAfterTextBG" src="/assets/images/Before-After/AfterTextBG.png" alt="">
+			<img class="layer layerMirror" src="/assets/images/Before-After/mirror.png" alt="">
+			<img class="layer layerScale" src="/assets/images/Before-After/scale.png" alt="">
+			<img class="layer layerJillAfter" src="/assets/images/Before-After/AfterJill.png" alt="">
+			<img class="layerJillBefore" src="/assets/images/Before-After/beforeJill.png" alt="">
+			<img class="layer layerBeforeTextFG" src="static/assets/images/Before-After/BeforeTextFG.png" alt="">
+		</div>
+		<div class="textWrap">
+			<p>{intro.text1}</p>
+			<p>{intro.text2}</p>
+			<p>{intro.text3}</p>
+			<div class="signature">
+				Jill Myburgh
+			</div>
+		</div>
 	</div>
-</section>
-
-<section class="introAfter">
-	<img class="imgAfter" src="{intro.imageAfter}" alt="Jill Myburgh in a bikini on stage looking like a super-model after her weight-loss">
+	
 </section>
 
 <!-- ******CSS -->
+<!-- NOT mobile first -->
 <style lang="scss">
-	section.introTitle {
+	section.intro {
 		padding: 3vw;
-		h1 {
-			margin: 0;
-			color: hsl(var(--onNeutral) / var(--onNeutralStrength3));
-			text-align: center;
-			font-family: $font_boldEmphasis;
-		}
-		.subtitle {
-			color: hsl(var(--onNeutral) / var(--onNeutralStrength2));
-			margin: 0.5em 0;
-			text-align: center;
-			font-family: $font_cursiveEmphasis;
-		}
-	}
-	section.introBefore {
 		position: relative;
-		overflow: hidden;
-		// min-height: 100%;
-		// height: 100%;
 		width: 100%;
-		display: grid;
-		grid-template-columns: 1fr 2rem 1fr;
-		
-		.graphicsWrap {
-			// border: 1px solid red;
-			position: relative;
-			display: flex;
-			justify-content: flex-start;
-			align-items: flex-end;
-			grid-column: 1 / span 2;
-			grid-row: 1;
-			.layer {
-				position: relative;
-				width: 100%;
-				height: auto;
-				
-				&.beforeLayerBG, &.beforeLayerMirror, &.beforeLayerScale {
-					position: absolute;
-				}
+
+		.introTitle {
+			padding: 6vw 0;
+			h1 {
+				margin: 0;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength3));
+				text-align: center;
+				font-family: $font_boldEmphasis;
+			}
+			.subtitle {
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength2));
+				margin: 0.5em 0;
+				text-align: center;
+				font-family: $font_cursiveEmphasis;
 			}
 		}
 		
 		.content {
-			border: 1px solid red;
-			padding: 3vw;
-			color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
 			display: flex;
-			flex-direction: column;
-			justify-content: flex-start;
-			align-items: center;
-			grid-column: 2 / span 2;
-			grid-row: 1;
-			z-index: 2;
-			:global(p) {
-				margin: 0;
-			}			
-		}
-		:global(.signature) {
-			color: hsl(var(--accentHS) var(--accentL));
-			font-family: $font_cursiveEmphasis;
-			font-size: var(--fontSizeH3);
-			margin: 0;
+			.graphicsWrap {
+				flex: 1.5;
+				position: relative;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				.layer {
+					position: absolute;
+					width: 100%;
+					height: auto;
+					max-height: 100%;
+					&.layerJillAfter, &.layerAfterTextBG {
+						visibility: hidden;
+					}
+				}
+				.layerJillBefore {
+					position: relative;
+					width: 100%;
+				}
+			}
+			.textWrap {
+				flex: 1;
+				color: hsl(var(--onNeutral) / var(--onNeutralStrength0));
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: flex-start;
+				padding: 3vw;
+				.signature {
+					color: hsl(var(--accentHS) var(--accentL));
+					font-family: $font_cursiveEmphasis;
+					font-size: var(--fontSizeH3);
+					margin: 0;
+				}		
+			}
 		}
 	}
-	section.introAfter {
-		background-color: transparent;
-		z-index: -1;
+
+	// Responsive Styles - for mobile
+	@media screen and (max-width: calc($mdScreen - 1px)) {
+		section.intro {
+			.content {
+				flex-direction: column-reverse;
+				.graphicsWrap {
+					flex: unset;
+					overflow: hidden;
+				}
+				.textWrap {
+					flex: unset;
+					margin: 0 auto;
+					max-width: 60ch;
+					padding: 0 0 6vw 0;
+				}
+			}
+		}
 	}
 </style>
 
@@ -103,33 +122,37 @@
 	
 	onMount(() => {
 		gsap.registerPlugin(ScrollTrigger);
+		gsap.set(".graphicsWrap *",{transformOrigin:"center center"});
+
 		let tl1 = gsap.timeline({
 			scrollTrigger: {
 				scroller: "main",
-				trigger: '.introBefore',
-				anticipatePin: 1,
+				trigger: '.intro .content',
 				scrub: 1,
 				start: "top 70%",
-				end: "bottom top",
-				pinSpacing: false
+				end: "center center"
 			}
 		});
-		tl1.from(".beforeLayerBG",{duration:1, rotate:40},"<");
-		tl1.from(".beforeLayerMirror",{duration:1, xPercent:-10},"<+0.1");
-		tl1.from(".beforeLayerScale",{duration:1, rotate:20, xPercent:-10},"<+0.1");
-		
+		tl1.from(".layerBG",{duration:1, rotate:40},"<");
+		tl1.from(".layerBeforeTextBG, .layerBeforeTextFG",{duration:1, rotate:40},"<");
+		tl1.from(".layerMirror",{duration:1, xPercent:-10},"<+0.1");
+		tl1.from(".layerScale",{duration:1.3, rotate:20, xPercent:-10},"<");
+
 		let tl2 = gsap.timeline({
 			scrollTrigger: {
 				scroller: "main",
-				trigger: '.introBefore .content',
-				endTrigger: '.introAfter',
+				trigger: '.intro .content .graphicsWrap',
+				scrub: 1,
 				start: "center center",
-				end: "bottom bottom",
-				pin: true,
-				pinSpacing: false,
-				anticipatePin: 1,
-				scrub: 1
+				end: "+=500px",
+				pin: ".intro .content",
+				anticipatePin: 1
 			}
 		});
+		tl2.from(".layerJillAfter",{duration:1, left:-300, autoAlpha:0},"<");
+		tl2.to(".layerMirror",{duration:1, left:-300, autoAlpha:0},"<0.5");
+		tl2.fromTo(".layerScale",{rotate:0, left:0, autoAlpha:1}, {duration:1.3, left:-300, rotate:-30, autoAlpha:0},"<0.5");
+		tl2.from(".layerAfterTextBG",{duration:1, right:-30, autoAlpha:0},"<0.5");
+		tl2.fromTo(".layerBG",{rotate:0}, {duration:1.3, rotate:90},"<");
 	});
 </script>
