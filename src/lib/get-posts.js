defc86e82872afae6dacd9fd27ae413fdf819a30
DonavanGrobler <donavangrobler2@gmail.com>
2022-01-09 12:08:11 +0200

    - Created a sitemap.xml.js File. - Created a get-posts.js File under the lib folder - Followed the "Sitemap toturial".

---
+++ src/lib/get-posts.js
@@ -0,0 +1,11 @@
+export async function getPosts() {
+    const posts = await Object.entries(
+      import.meta.globEager('/posts/**/*.md')
+    )
+      // get post metadata
+      .map(([, post]) => post.metadata)
+      // sort by date
+      .sort((a, b) => (a.date < b.date ? 1 : -1))
+  
+    return posts
+  }
